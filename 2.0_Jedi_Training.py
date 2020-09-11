6b5bb3874390e02fbbe851b5ce6979a903709949
thawngbaw <thawngbaw@urbandaleschools.com>
2020-09-11 11:01:28 -0500

    jedi training 2

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,5 +1,5 @@
 '''
-Sign your name:________________
+Sign your name:___Bawi Nun Thawng___
 
 1.) How do you enter a single line comment in a program? Give an example.
 
@@ -7,15 +7,15 @@ Sign your name:________________
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = 7
+type(42)            = <class 'int'>
+type(42.0)          = <class 'float'>
+type("C3PO")        = <class 'str'>
+type(True)          = <class 'bool">
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,8 +26,8 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = <class 'float'>
 
 
 4.) What is the mistake in the following code. Fix it!
@@ -35,13 +35,17 @@ type(a)       =
 x,y = (4,5)
 a = 3(x + y)
 a
-
+x,y= (4,5)
+a = 3*(x + y)
+a
+27
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
 x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
-
+ x,y,z =(3,4,5)
+ ave = (x+y+z)//3
 
 '''
